e4abed0f952b96f94d4133856b980da5851d6c9e
Paul Beckwith <303dog@gmail.com>
2019-08-12 00:51:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  #attr_reader :brand
 
   BRANDS = []
   
@@ -14,7 +14,7 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  def brand
+  def brand(brand)
      brands.each do |brand|
   end 
 end 
